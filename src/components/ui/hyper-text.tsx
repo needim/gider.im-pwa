0ba15074af2c0ac2edb5c6a69ac9b6893416e8ca
Nedim Arabacı <nedimarabaci@gmail.com>
2024-10-03 00:14:11 +0300

    Update duration in HyperText component

--- src/components/ui/hyper-text.tsx
+++ src/components/ui/hyper-text.tsx
@@ -19,7 +19,7 @@ const getRandomInt = (max: number) => Math.floor(Math.random() * max);
 
 export default function HyperText({
 	text,
-	duration = 100,
+	duration = 1000,
 	framerProps = {
 		initial: { opacity: 0, y: -10 },
 		animate: { opacity: 1, y: 0 },
